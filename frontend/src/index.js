473425db7b4e74867292b2aee797c12d238f4c8a
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2020-03-07 21:25:32 -0300

    wip

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 import App from "./main/App"
-import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux"
+ReactDOM.render(<App />, document.getElementById('root'))
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.unregister();
